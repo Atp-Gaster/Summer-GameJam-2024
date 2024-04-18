7afb3ef8e22f8953eaf4e69d72b09b2e53a78b74
Dragon <nawatng@gmail.com>
2024-04-18 17:37:08 +0700

    [Update] Hand Icon drag and drop + player sprite demo

--- Assets/Demo Assets/Script/TemperatureBar.cs
+++ Assets/Demo Assets/Script/TemperatureBar.cs
@@ -8,6 +8,8 @@ public class TemperatureBar : MonoBehaviour
     public Slider slider;
     public float totalTime = 60f; // Total time for the countdown in seconds
     [SerializeField] float currentTime; // Current time in the countdown
+    [SerializeField] Image PlayerPicture;
+
 
     void Start()
     {
@@ -30,11 +32,12 @@ public class TemperatureBar : MonoBehaviour
             {
                 currentTime = totalTime;
                 // Optionally, you can add code here to handle what happens when the countdown reaches the total time
+               
             }
         }
-        else
-        {
-            Debug.LogWarning("Slider reference is missing!");
-        }
+
+        if (currentTime >= 25) PlayerPicture.color = Color.yellow;
+        if (currentTime >= 50) PlayerPicture.color = new Color(1.0f, 0.64f, 0.0f);
+        if (currentTime >= 75) PlayerPicture.color = Color.red;
     }
 }
